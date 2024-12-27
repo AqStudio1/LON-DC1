0a1c4926baab4350cf80d96a4bcc6515f5ed32d4
AqStudio1 <zaghum.mughal.1@gmail.com>
2024-12-27 10:45:11 +0000

    Add files via upload

---
+++ Labfiles/Support/CopyMonitor.bat
@@ -0,0 +1,5 @@
+md c:\Monitor
+
+copy \\lon-dc1\labfiles\Support\MonitorScenario.vbs c:\Monitor
+
+copy \\lon-dc1\labfiles\Support\MonitorScenario.cmd c:\Monitor
---
+++ Labfiles/Support/CopyUserData.bat
@@ -0,0 +1,11 @@
+md c:\Data
+
+copy \\lon-dc1\labfiles\Support\Data c:\Data
+
+md c:\Misc
+
+copy \\lon-dc1\labfiles\Support\Misc c:\Misc
+
+md c:\Reports
+
+copy \\lon-dc1\labfiles\Support\Reports c:\Reports
